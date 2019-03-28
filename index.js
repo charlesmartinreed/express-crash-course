119c4c32a7dc6b1a01d24fa45846016c90f091c7
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-28 17:57:10 -0500

    First commit, basic express and node setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+			app = express(),
+			port = process.env.PORT || 5000;
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+	// res.send('<h1>Hello world!... From nodemon!</h1>');
+
+	// you can load HTML files this way, but we'd have to have separate routes for each page. This isn't ideal.
+	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+	// instead, we want to make the public folder itself static with app.use
+});
+
+// use is the method we utilize when we want to implement middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+	console.log(`Server now running on port ${port}`);
+});
