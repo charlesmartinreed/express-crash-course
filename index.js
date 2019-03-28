67da769a3133ff7b873aa193aa8fb0c65087321d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-28 18:07:15 -0500

    Demonstrate basic example of serving up pages with Express, using static middleware.

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const express = require('express'),
 const path = require('path');
 
 app.get('/', (req, res) => {
-	// res.send('<h1>Hello world!... From nodemon!</h1>');
-
-	// you can load HTML files this way, but we'd have to have separate routes for each page. This isn't ideal.
+// 	// res.send('<h1>Hello world!... From nodemon!</h1>');
+//
+// 	// you can load HTML files this way, but we'd have to have separate routes for each page. This isn't ideal.
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 
-	// instead, we want to make the public folder itself static with app.use
+// 	// instead, we want to make the public folder itself static with app.use
 });
 
-// use is the method we utilize when we want to implement middleware
+
+
+// use is the method we utilize when we want to implement middleware. This static middleware is provided by express itself.
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
